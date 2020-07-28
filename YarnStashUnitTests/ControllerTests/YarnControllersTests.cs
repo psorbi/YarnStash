746dba18916a8cf8c24905c0ed94ef7c0fa1037c
psorbi <p.sorbi@gmail.com>
2020-07-27 20:27:08 -0400

    started working on unit testing

---
+++ YarnStashUnitTests/ControllerTests/YarnControllersTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using YarnStash.Controllers;
+using YarnStash.Data;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace YarnStashUnitTests.ControllerTests
+{
+    public class YarnControllersTests
+    {
+        private readonly YarnController _yarnController;
+
+        public YarnControllersTests()
+        {
+            var contextOptions = new DbContextOptionsBuilder<YarnContext>().UseInMemoryDatabase("inMemoryDatabase").Options;
+            var context = new YarnContext(contextOptions);
+            _yarnController = new YarnController(context);
+        }
+
+        [Fact]
+        public async void Details_NullID_ReturnNoContent()
+        {
+            //Arrange
+
+            //Act
+            var result = await _yarnController.Details(null);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
